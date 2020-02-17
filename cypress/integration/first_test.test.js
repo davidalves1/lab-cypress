7617142d16eaaa800eb81154ab3ed56db4a7d5f6
David Alves de Souza <alves.david@outlook.com>
2020-02-17 13:51:10 -0300

    Initial commit

---
+++ cypress/integration/first_test.test.js
@@ -0,0 +1,25 @@
+describe('My First Test', () => {
+  it('should be a h1 element', () => {
+    cy.visit('http://localhost:8080');
+
+    cy.get('h1')
+      .should('have.text', 'Hello Cypress!');
+  });
+
+  it('should be a span with input value', () => {
+    cy.visit('http://localhost:8080');
+    const name = 'David';
+    const surname = 'Alves';
+
+    cy.get('#txtName')
+      .type(name)
+      .should('have.value', name);
+
+    cy.get('#txtSurname')
+      .type(surname)
+      .should('have.value', surname);
+
+    cy.get('#myName')
+      .should('have.text', `${name} ${surname}`);
+  })
+})
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Hello Cypress!</h1>
+    <input type="text" id="txtName" v-model="name" placeholder="name">
+    <input type="text" id="txtSurname" v-model="surname" placeholder="surname">
+    <h2 id="myName">{{ myName }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: '',
+      surname: ''
+    }
+  },
+  computed: {
+    myName() {
+      return `${this.name} ${this.surname}`;
+    }
+  }
+}
+</script>
